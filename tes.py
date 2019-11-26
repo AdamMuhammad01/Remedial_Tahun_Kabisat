d4b672d3d46f57980a2c8b43b9f428b2b3473059
AdamMuhammad01 <adam.muhammad92@gmail.com>
2019-11-26 15:47:48 +0700

    checkpoint level 1

---
+++ tes.py
@@ -0,0 +1,30 @@
+# staging area => add
+# commit => commit
+# git init                                                       ###
+# git config --global user.name aduh
+# git config --global user.email aduh@gmail
+# git add .                                                      ###
+# git commit -m "checkpoint level 1"                             ###
+
+
+# git status
+# git rm --cached cobalagi.py
+
+# git log  (cek komit)
+# git log --oneline (cek komit yg penting)
+# git checkout master
+# git checkout 43678ca(dpt dr oneline)
+# git reset 43678ca
+
+
+# git branch cihuy(nama yg diinginkan)
+# git branch -a (cek branch yg dimiliki)
+# git checkout -b cihuy(switch ke cihuy)
+# git checkout cihuy (switch master ke branch)
+# git checkout master (ngecek lokasi apa sudah di master)
+# git branch -D cihuy (menghapus branch)
+# git merge cihuy(merge with master)
+
+# git remote add cobalagi(namafile) https://github.com/AuliaAPratama/PythonBasic.git      ###
+# git push cobalagi(namafile) master                                                      ###
+# git clone link
