78be27a1af86c68de4b7ff5456d1a1cfd4f5e7d1
AdamMuhammad01 <adam.muhammad92@gmail.com>
2019-11-27 15:04:50 +0700

    checkpoint

--- github.py
+++
@@ -1,5 +0,0 @@
-git init   
-git add .                                                      ###
-git commit -m "checkpoint level 1" 
-git remote add ExamRemed(namafile) https://github.com/AdamMuhammad01/JCDSExamRemed.git      ###
-git push ExamRemed(namafile) master  
